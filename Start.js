0d491af7d8548fd7a2f2b7d2e7326c5fba969dbd
Gwparsa <Gwparsa@gmail.com>
2022-03-02 00:30:28 +0330

    uploaded #1

---
+++ Start.js
@@ -0,0 +1,52 @@
+// Writed by GwParSa - https://GitHub.com/GwParSa - Lang: Persian
+
+// Introduction:
+  // JS is most popular programming language. it is the language of the web and now well beyond the web. also, we can use JS in robots, gaming etc as well. but JS is a native web programming language.
+
+
+// Story of JS:
+  // dar sal 1995 avalin version js dar 10 rooz be vasile Brandon Eich tarahi shod. in zaban ebteda Mocha nam dasht va sepas be JavaScript taghir peyda kard.
+  // hamchenin dar saal haye aval js shakhsi be nam Douglas Crockford be pishraft in zaban komak kard va dar mored an ketabi ba nam JavaScript:The Good Parts nevesht.
+
+
+// JavaScript Versions:
+  // ECMA : Europen Computer Manfacture Association ya haman anjoman tolid computer europa ast ke version haye standard ziadi baraye JS montasher kard:
+  // ECMAscript1 -> in 1997
+  // ECMAscript2 -> never released!
+  // ECMAscript3 -> in 1999
+  // ECMAscript4 -> never released!
+  // ECMAscript5 -> in 2009 and become king of the Web
+  // ECMAscript6 -> in 2015 and become the most popular version (ES6)
+  // ECMAscript7 -> in 2016
+  // ECMAscript8 -> in 2017
+  // ECMAscript9 -> in 2018
+
+
+// Comment:
+  // mitavan yek comment baraye tozihat ba // dar aval khat sakht.
+  
+
+// Variables:
+  // variables are like containers that hold somthing temporarily.
+  //Anatomy:
+    var chesse = "yummy";
+    var age = 25;
+  // yummy baraye cheese be soorat movaghat taarif shode.
+  // dar mesal dovom addad 25 dar quotes("") gharar nagerefte chon yek adad ast va adad ha dar JS taarif shde hastand ama kalamat kheyr!
+
+    // dar variables ha nmitavanim az name haye zir estefade konim:
+      var 1test ="test"; // ba adad shoru nmishavand.
+      var $test ="test"; // ba alamat haye %^&$ etc shoru nmishavand.
+      var test-test ="test"; // nmitavan beyn name az - estefade kard.
+      var test test ="test"; // nmitavan az space bein name estefade kard.
+      var new ="test"; // nmitavan az keyword haye js baraye name estefade kard.
+  
+        // bozorgi va choochaki dar name var mohem ast va ba ham motefavet hastand:
+          var name ="";
+          var Name ="";
+          var nAme ="";
+          var Myname ="";
+          var MyName ="";
+          var myName ="";  // ...
+
+
