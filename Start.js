39889909133d58493cbd1a9286c5ebde24635a24
Gwparsa <Gwparsa@gmail.com>
2022-04-09 13:27:04 +0430

    uploaded #6

--- Start.js
+++ Start.js
@@ -95,3 +95,79 @@
   // mitavan ba method sort yek array ra be soorat horoof alefba chid:
     vehicles.sort();
   
+
+// Oprators, Booleans and Selections :
+  //oprator ha shamel symbol hayi hastand ke harkodam amaliat mokhtalefi ra shekl midahand, mesl: = , == , >< , + , - , / va ... 
+  //Boolean ha be noei data gofte mishavad ke ya true ast ya false ! pas meghdar dataye boolean ha true , false ast. hamchenin 0, null, undefined, NaN dar boolean ha be manaye false hastand.
+  //dar selection ma mitavanim baraye boolean ha shart true ya false bogzarim etc..
+
+
+//Comparing Values: 
+  //Dar JS == be manaye "mesle.." ast yani dar shorut agar bein do variable in oprator ra bogzarim be manaye yeksan boodan anha ast:
+    if (myNum == otherNum) {}
+  //hamchenin != be manaye gheir yeksan boodan an do variable ast :
+    if (myNum != otherNum) {}
+  //hamchenin < ya > be manaye bozorg tar ya koochik tar boodan value yek variable ast (bishtar dar number ha estefade mishavad):
+    if (myNum > otherNum) {}
+    if (myNum < otherNum) {}
+  // **Type Coercion: vaghti do variable ba value haye gheir ham noe masalan string va number dar shorut ba == test mishavand, test an true ast zira dar in statement noe value mohem nmibashad balke khode value mohem ast:
+    var myNum = 5;
+    var notNum = "5";
+    if (myNum == notNum) {} // true...
+  //Dar ES5 (2009) oprator === be JS add shod, ke baraks mored bala amal mikonad va noe value ham baraye in oprator mohem ast:
+    var myNum = 5;
+    var notNum = "5";
+    if (myNum == notNum) {} // false...
+  
+
+//Else If:zamani ke ma yek statement sharti darim az if va else estefade mikonim ama agar bekhahim shart ma option haye bishtari dashte bashad mitavan da bein if va else yek Else If gharar dad: 
+  var colors = ['blue', 'red', 'green', 'yellow'];
+  var selectedColor = colors[1]; // Dar inja yeki az rang haye colors ro entekhab va dar statement paiin mikhahim peydayash konim:
+
+  if (selectedColor == colors[0]) {
+    console.log("rang entekhabi: " + colors[0]);
+  } else if(selectedColor == colors[1]) {
+    console.log("rang entekhabi: " + colors[1]);
+  } else if(selectedColor == colors[2]) {
+    console.log("rang entekhabi: " + colors[2]);
+  } else {
+    console.log("rang entekhabi: " + colors[3]);
+  }
+  
+
+//Switch:
+  //gahi oghat shart haye ma be ghadri ziad ast ke nmitavanim az if va else estefade konim pas mitavanim az switch statement estefade konim :
+  switch(selectedColor){
+      case colors[0]: console.log("rang shoma: " + colors[0]); break; //baraye inke vaghti statement ma yek javab true gereft az kol statement kharej beshe va dobare amal tekrar nashe az break; estefade mikonim.
+      case colors[1]: console.log("rang shoma: " + colors[1]); break;
+      case colors[2]: console.log("rang shoma: " + colors[2]); break;
+      case colors[3]: console.log("rang shoma: " + colors[3]); break;
+    default: console.log("Rang shoma dar in list nist !"); // zamani ke shart ma dar statement vojud nadashte bashe be soorat default in khat code ejra mishe. 
+  }
+
+
+// && and || oprators:
+  // vaghti bekhahim dar statement haye sharti bekhahim do variable ro check konim ke bebinim aya har do true hastand, bein anha && gharar midahim:
+    var goodmood = true;
+    var gotsleep = true;
+
+    if(goodmood && gotsleep) {
+      console.log("emrooz rooz khoobie ;)")
+    } else {
+      console.log("emrooz halam khosh nist ;(")
+    }
+
+  //ama agar bekhahim bebinim ke aya hata yeki az anha true ast, bayad az || dar beineshan estefade konim:
+    var goodmood = true;
+    var gotsleep = true;
+    var breakfast = false;
+
+    if(goodmood || gotsleep || breakfast) { //faghad breakfast false ast ama do mored digar true hastand.
+      console.log("emrooz rooz khoobie ;)")
+    } else {
+      console.log("emrooz halam khosh nist ;(")
+    }
+
+
+//Loops:
+  
