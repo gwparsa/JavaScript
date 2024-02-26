4f1d831b7b3ea0279bb22178e11710c50b4629dd
Gwparsa <Gwparsa@gmail.com>
2024-02-26 13:58:59 +0330

    challenge 1 changed

--- challenge_1.js
+++ challenge_1.js
@@ -1,3 +1,5 @@
+//se kala ra mikhaim bekharim. meghdar pooleman ra darim. baad az fahmidan total price, average price va final price baad az takhfif baressi mikonim ke aya mitavanim an ha ra ba pooleman bekharim ya na.
+
 const price1 = 10;
 const price2 = 11;
 const price3 = 28;
@@ -10,4 +12,4 @@ console.log("Average Price is: " + avgPrice);
 const finalPrice = totalPrice * 0.95;
 console.log("Final Price is: " + finalPrice);
 
-const canBuy = balance >= finalPrice ? console.log("Nice! You've Enough Balance To Place This Order!") : console.log("What a Pitty! You Dont Have Enough Money!");
+const canBuy = balance >= finalPrice ? console.log("Nice! You've Enough Balance To Place This Order!") : console.log("What a Pitty! You Dont Have Enough Money!"); // --> Nice! You've Enough Balance To Place This Order!
