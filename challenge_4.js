ddeae9fc6c5fd6a6bd2f0ad6c4efe5793757f6da
Gwparsa <Gwparsa@gmail.com>
2024-03-04 15:27:00 +0330

    challange 4 updated

--- challenge_4.js
+++ challenge_4.js
@@ -7,3 +7,16 @@ const lName = name.slice(spaceIndex + 1);
 
 const text = `Hello. My fisrt name is ${fName} and my last name is ${lName}.`;
 console.log(text); // --> Hello. My fisrt name is Parsa and my last name is Sorsori.
+
+
+
+
+
+
+//hala baad az yadgiri array ha in tamrin ra hal mikonim:
+
+const name2 = "Parsa Sorsori";
+const nameArray = name2.split(" ");
+
+const text2 = `Hello. My fisrt name is ${nameArray[0]} and my last name is ${nameArray[1]}.`;
+console.log(text2); // --> Hello. My fisrt name is Parsa and my last name is Sorsori.
