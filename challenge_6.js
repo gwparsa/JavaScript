266c74940fab107400f9ff0f7b1e5b46b7cb0ce0
Gwparsa <Gwparsa@gmail.com>
2024-03-13 01:04:47 +0330

    cjallenge 6 done

---
+++ challenge_6.js
@@ -0,0 +1,7 @@
+//array zir ra yek bar destructure konam va be do bakhsh tabdil konim va khod an ha ra ham dobare destructure konim va be ajzaye koochek tar tabdil konim.
+
+const fullStack = [["HTML", "CSS", "JS", "React"], ["Node", "Express", "MangoDB"]]
+const [frontEnd, backEnd] = fullStack;
+
+const [html, css, js, react] = frontEnd;
+const [node, express, mangodb] = backEnd;
